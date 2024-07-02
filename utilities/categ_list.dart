679983611bac63003744ad3944ac12981c774136
SudoTech369 <hoodsofts@gmail.com>
2024-07-02 13:44:02 +0300

    Add files via upload

---
+++ utilities/categ_list.dart
@@ -0,0 +1,141 @@
+List<String> maincateg = [
+'select category',
+  'Chairs',
+  'Desks',
+  'Coffee Tables',
+  'Safes',
+  'Tv Stands',
+  // 'Wardrobes',
+  'Cabinets',
+  // 'Kids',
+  // 'Beauty'
+];
+
+List<String> chairs = [
+'subcategory',
+  'Executive',
+  'Ergonomic',
+  'Leather',
+  'Mesh',
+  'Guest',
+  'Armless',
+  'Stools',
+  'Kneeling',
+  'High-Back',
+];
+List<String> desks = [
+'subcategory',
+  'Executive',
+  'Managerial',
+  'Workstation',
+  'L-Shaped',
+  'U-Shaped',
+  'Reception',
+  'Conference',
+  'Adjustable',
+  'Corner'
+];
+List<String> stand = [
+'subcategory',
+  'Glass',
+  'Rustic',
+  'Corner',
+  'Swivel',
+  'Floating',
+  'E-Fireplace',
+  'Cabinet',
+  'Metal',
+  'Wood',
+  
+  // 'keyboard',
+  // 'gaming',
+  // 'other'
+];
+
+List<String> safe = [
+'subcategory',
+  'Fireproof',
+  'Burglary',
+  'Depository',
+  'Biometric',
+  'Electronic',
+  'Document',
+  // 'men snadals',
+  // 'home slippers',
+  // 'women slippers',
+  // 'women boots',
+  // 'women heels',
+  // 'women sport',
+  // 'women snadals',
+  // 'other'
+];
+
+// List<String> stand = [
+// 'subcategory',
+//   'living room',
+//   'bed room',
+//   'dinning room',
+//   'kitchen tools',
+//   'bath access.',
+//   'furniture',
+//   'decoration',
+//   'lighting',
+//   'garden',
+//   'other'
+// ];
+
+// List<String> beauty = [
+// 'subcategory',
+//   'body care',
+//   'hair care',
+//   'men perfume',
+//   'women perfume',
+//   'make up',
+//   'other'
+// ];
+
+List<String> tables = [
+'subcategory',
+  'Conference',
+  'Breakroom',
+  'Training',
+  'Collabo',
+  'Round',
+  'Square',
+  'Rectangular',
+  // 'necklace',
+  // 'scarf set',
+  // 'anklet',
+  // 'other'
+];
+
+// List<String> kids = [
+// 'subcategory',
+//   'girls sets',
+//   'girls dress',
+//   'girls top',
+//   'girls pants',
+//   'jacket',
+//   'sweatshirts',
+//   'boys sets',
+//   'boys top',
+//   'boys pants',
+//   'home wear',
+//   'boys suits',
+//   'baby shoes',
+//   'other'
+// ];
+
+List<String> cabinets = [
+  'subcategory',
+  'Filling',
+  'Storage',
+  'Credenza',
+  'Mobile',
+  'Overhead',
+  'Lockable',
+  
+  'Corner',
+  'Cupboard'
+  
+];
